2b5cd83e02bb2951824942edfd95d5ef33c2564d
fhammache <faredj.hammache@particeep.com>
2021-09-09 12:05:29 +0200

    test

--- build/TestComponent/TestComponent.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from "react";
-export interface TestComponentProps {
-    heading: string;
-    content: ReactNode;
-}
-import "./TestComponent.scss";
-declare const TestComponent: React.FC<TestComponentProps>;
-export default TestComponent;
